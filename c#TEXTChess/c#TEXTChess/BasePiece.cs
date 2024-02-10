29b8d8249d7bba72dd0b1ebc343200a1a3c4df99
pg26max@vfs.com <pg26max@vfs.com>
2024-02-09 23:12:29 -0800

    Started on the BasePiece class

--- c#TEXTChess/c#TEXTChess/BasePiece.cs
+++ c#TEXTChess/c#TEXTChess/BasePiece.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace c_TEXTChess
 {
+    enum ETeam
+    {
+        White = 0,
+        Black
+    }
+
+
     internal class BasePiece
     {
+        public ETeam team { private set; get; }
+
+        public virtual bool Move()
+        {
+
+
+
+            return true;
+        }
     }
 }
