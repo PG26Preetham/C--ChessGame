6e1c9613181afc10c302098153338a35074d38ec
PG26Preetham <PG26Preetham@vfs.com>
2024-02-09 22:42:37 -0800

    Added the project

---
+++ c#TEXTChess/c#TEXTChess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_TEXTChess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
