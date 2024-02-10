bbf64c854d4e1ae7684ad57ea0bfc5fcc6ba8894
pg26max@vfs.com <pg26max@vfs.com>
2024-02-09 23:37:54 -0800

    (+) Created virtual functions for BasePiece class, Grid struct, Started on Knight class

--- c#TEXTChess/c#TEXTChess/BasePiece.cs
+++ c#TEXTChess/c#TEXTChess/BasePiece.cs
@@ -14,20 +14,29 @@ namespace c_TEXTChess
 
     public struct Grid
     {
-
+        public int x;
+        public int y;
     }
-
 
     internal class BasePiece
     {
         public ETeam team { private set; get; }
+        public Grid currentPos;
 
-        public virtual bool Move()
+        public void Initialize(ETeam team, Grid startPos)
         {
+            this.team = team;
+            currentPos = startPos;
+        }
 
-
-
+        public virtual bool Move(Grid startPos, Grid endPos)
+        {
             return true;
+        }
+
+        public virtual Grid[] GetLegalMoves()
+        {
+            return new Grid[2];
         }
     }
 }
---
+++ c#TEXTChess/c#TEXTChess/Knight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_TEXTChess
+{
+    internal class Knight : BasePiece
+    {
+
+
+        public override bool Move(Grid startPoint, Grid endPoint)
+        {
+
+
+            return true;
+        }
+
+        public override Grid[] GetLegalMoves()
+        {
+
+
+
+            return new Grid[2];
+        }
+    }
+}
