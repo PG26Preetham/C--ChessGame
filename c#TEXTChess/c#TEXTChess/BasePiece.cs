10c0b357a625dbcf3938e37c30786017d072b8a8
pg26max@vfs.com <pg26max@vfs.com>
2024-02-10 02:10:59 -0800

    (+) New CheckBounds() function inside BasePiece class, Created and implemented GetLegalMove() override functions for Knight, Bishop, Rook, and Queen

--- c#TEXTChess/c#TEXTChess/BasePiece.cs
+++ c#TEXTChess/c#TEXTChess/BasePiece.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using c_TEXTChess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,29 +16,67 @@ namespace c_TEXTChess
 
     public struct Grid
     {
+       
         public int x;
         public int y;
+
+        public Grid Initialize(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            return this;
+        }
     }
 
     internal class BasePiece
     {
         public ETeam team { private set; get; }
-        public Grid currentPos;
+        public Grid currentPos { private set; get; }
 
+    
         public void Initialize(ETeam team, Grid startPos)
         {
             this.team = team;
             currentPos = startPos;
         }
 
-        public virtual bool Move(Grid startPos, Grid endPos)
+        // I feel like we can remove the "startPos" because it would just be the "currentPos"
+        public virtual bool Move(Grid startPos, Grid endPos) { return true; }
+
+
+        // Virtual function for finding all legal moves
+        public virtual List<Grid> GetLegalMoves() { return new List<Grid>(0); }
+
+
+        // Function for checking if the moves are within the board
+        protected List<Grid> CheckBounds(List<Grid> legalMove)
         {
-            return true;
+            for (int i = legalMove.Count - 1; i > 0; i--) // Iterating backwards to prevent issues caused by RemoveAt()
+            {
+                if (legalMove[i].x < 1 || legalMove[i].x > 8 || legalMove[i].y < 1 || legalMove[i].y > 8)
+                {
+                    legalMove.RemoveAt(i);
+                }
+            }
+
+            return legalMove;
         }
 
-        public virtual Grid[] GetLegalMoves()
-        {
-            return new Grid[2];
-        }
+
     }
 }
+
+// Put this in {Program.cs} to test
+
+/*BasePiece testPiece = new Rook();
+testPiece.Initialize(ETeam.Black, new Grid().Initialize(4, 4));
+
+List<Grid> legalMoves = testPiece.GetLegalMoves();
+
+for (int i = 0; i < testPiece.GetLegalMoves().Count; i++)
+{
+    Console.Write(legalMoves[i].x);
+    Console.Write(" , ");
+    Console.WriteLine(legalMoves[i].y);
+}*/
---
+++ c#TEXTChess/c#TEXTChess/Rook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_TEXTChess
+{
+    internal class Rook : BasePiece
+    {
+        public override bool Move(Grid startPoint, Grid endPoint)
+        {
+
+
+            return true;
+        }
+
+        public override List<Grid> GetLegalMoves()
+        {
+            List<Grid> legalMove = new List<Grid>();
+
+            for (int i = 1; i < 8; i++)
+            {
+                legalMove.Add(new Grid().Initialize(currentPos.x + i, currentPos.y));
+                legalMove.Add(new Grid().Initialize(currentPos.x - i, currentPos.y));
+                legalMove.Add(new Grid().Initialize(currentPos.x, currentPos.y + i));
+                legalMove.Add(new Grid().Initialize(currentPos.x, currentPos.y - i));
+            }
+
+            legalMove = CheckBounds(legalMove);
+
+            return legalMove;
+        }
+    }
+}
